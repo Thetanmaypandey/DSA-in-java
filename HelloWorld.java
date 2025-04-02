a3311ae8f54baa86689cfd3563c7a4d2303c5a4f
Tanmay Pandey <152524669+Thetanmaypandey@users.noreply.github.com>
2025-04-02 11:01:57 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+// This program generally print the "Hello World!"
+public class HelloWorld
+   {
+     public static void main(String tanmays[])
+       {
+        System.out.println("Hello World!");
+
+       }
+   }
+/*
+    1.Every Java program must have at least one class. The class name should follow PascalCase naming convention. i.e. "public class HelloWorld"
+    2.Every Java program must have a main method, which serves as the entry point for execution.i.e. "    public static void main(String[] args)"
+    3.public: Allows the method to be accessible from anywhere.
+
+static: Belongs to the class, so it can be executed without creating an instance.
+
+void: Indicates that the method does not return any value.
+
+String[] args: Command-line arguments.
+
+
+
+
+
+*/
