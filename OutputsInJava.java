e3f11da925a21d48842e7277947c8deae8daeee6
Tanmay Pandey <152524669+Thetanmaypandey@users.noreply.github.com>
2025-04-02 21:01:43 +0530

    Add files via upload
    the ways how we can show the outputs

---
+++ OutputsInJava.java
@@ -0,0 +1,18 @@
+// Output  ways in java 
+import java.util.*;
+
+
+public class OutputsInJava
+{
+public static void main(String tanmay[])
+{
+    int roll = 10;
+    String name = " tanmay pandey";
+
+    System.out.println("the roll number is : " + roll +"and name is "+ name); // after printing the output this method will change the line
+    System.out.println("the roll number is : "+ roll + "and name is " +name + "\n");// this method doesn't change the control in the next line like "println" method.
+    System.out.printf("the roll number is : %d and name is %s \n " , roll , name);// like c langauge for formatted outputs
+    
+
+}
+}
